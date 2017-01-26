7ef61754bbb61a492b4abd7d3c4f4131f6e75322
mark <mark@ipp.ph>
2017-01-26 14:24:33 +0800

    update 01262017

--- resources/views/payments/result.blade.php
+++ resources/views/payments/result.blade.php
@@ -2,9 +2,10 @@
 @section('content')
 <div class="paysuccess">
 @if($result == "success")
-    <p><strong>Payment Success</strong></p>
+    <p><strong>決済完了しました。</strong></p>
+    <p><strong>ドライバー決定のメッセージが届きますので、アプリに戻ってお待ちください。</strong></p>
     @else
-    <p><strong>Payment Failure</strong></p>
+    <p><strong>決済に失敗しました。</strong></p>
     @endif
     <div><a class="btn btn-primary" href="http://back-to-app">アプリに戻る</a></div>
 @endsection
